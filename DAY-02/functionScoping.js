129c7c31884cd6682245b234d70ea84a76577747
unknown <gunjansahu3714@gmail.com>
2026-07-24 09:27:29 +0530

    Add functionScoping.js to DAY-02

---
+++ DAY-02/functionScoping.js
@@ -0,0 +1,10 @@
+console.log(greet);
+function greet(){
+    let section ="cse 18";
+    var subject ="FSD";
+    console.log('${section} ${subject}');
+}
+greet();
+console.log(greet);
+
+// console.log(subject);
